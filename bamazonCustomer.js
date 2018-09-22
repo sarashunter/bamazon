2e6e7b53ba7349869959dd6985ebac482e9ce169
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-22 13:14:25 -0400

    Create mysql connection and test

---
+++ bamazonCustomer.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error(`error connection: ${err}`);
+    }
+
+    console.log('connected!');
+});

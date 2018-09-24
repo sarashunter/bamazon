07a4630dacf5bd828402696cc9c4362aa72740c8
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-24 19:38:12 -0400

    Create Manager View and Menu

---
+++ bamazonManager.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('console.table');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889, //Replace with local port.
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error(`error connection: ${err}`);
+    }
+
+    // console.log('connected!');
+
+    //Copy products to an array for quick access.
+    connection.query("SELECT * FROM products", (err, res) => {
+        if(err) throw err;
+
+        managerMenu(res);
+    });
+
+});
+
+const managerMenu = (inventory) => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mainmenu',
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+            message: 'What would you like to do?'
+        }
+    ]).then(res => {
+        console.log(res);
+
+        switch (res.mainmenu){
+            case 'View Products for Sale':
+            viewProducts(inventory);
+            break;
+        }
+    })
+}
+
+const viewProducts = inventory =>{
+    console.log('viewing products');
+    console.table(inventory);
+    mainmenu(inventory);
+}

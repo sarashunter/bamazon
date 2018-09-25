d99440f7f3e91ced0cf3688be515702f44ac1219
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-24 21:05:02 -0400

    Calculate product sales.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -101,9 +101,10 @@ const promptCustomerForQuantity = product => {
 }
 
 const makePurchase = (product, quantity) => {
+    const thisSale = product.price * quantity;
     connection.query(
-        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
-        [quantity, product.item_id],
+        "UPDATE products SET stock_quantity = stock_quantity - ?, product_sales = product_sales + ?  WHERE item_id = ?",
+        [quantity, thisSale, product.item_id],
         function(err, res) {
           // Let the user know the purchase was successful, re-run loadProducts
           console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "'s!");
